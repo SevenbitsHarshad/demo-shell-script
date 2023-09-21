ba12542a45307b175f0e34dbff2ebbdd03ed781f
Harshad Patel <harshad@sevenbits.in>
2023-09-21 18:11:29 +0530

    added go dir

---
+++ go/pkg/mod/github.com/sei-protocol/sei-cosmos@v0.2.61/server/config/config_test.go
@@ -0,0 +1,25 @@
+package config
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func TestDefaultConfig(t *testing.T) {
+	cfg := DefaultConfig()
+	require.True(t, cfg.GetMinGasPrices().IsZero())
+}
+
+func TestSetMinimumFees(t *testing.T) {
+	cfg := DefaultConfig()
+	cfg.SetMinGasPrices(sdk.DecCoins{sdk.NewInt64DecCoin("foo", 5)})
+	require.Equal(t, "5.000000000000000000foo", cfg.MinGasPrices)
+}
+
+func TestSetSnapshotDirectory(t *testing.T) {
+	cfg := DefaultConfig()
+	require.Equal(t, "", cfg.StateSync.SnapshotDirectory)
+}
