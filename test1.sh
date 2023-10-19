96e3a5637599b99eb5211594df029234e3b9d8cb
SevenbitsHarshad <102015074+SevenbitsHarshad@users.noreply.github.com>
2023-10-19 19:56:27 +0530

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -4,5 +4,6 @@ sudo chown -R sxt-admin:sxt-admin /home/sxt-admin/demo-shell-script
 sudo chmod -R 775 /home/sxt-admin/demo-shell-script
 sudo chown -R sxt-admin:sxt-admin /home/sxt-admin/demo-shell-script/sei
 sudo chmod -R 775 /home/sxt-admin/demo-shell-script/sei
+git config --global --add safe.directory /home/sxt-admin/demo-shell-script/sei
 cd /home/sxt-admin/demo-shell-script/sei
 git checkout v3.1.1
