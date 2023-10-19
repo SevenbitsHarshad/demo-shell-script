476b213867f8fa11fa71407331413dd1bff28aeb
SevenbitsHarshad <102015074+SevenbitsHarshad@users.noreply.github.com>
2023-10-19 19:46:09 +0530

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 git clone https://github.com/sei-protocol/sei-chain/ sei
-sudo chown -R sxt-admin:sxt-admin /home/sxt-admin/sei
-sudo chmod -R 775 /home/sxt-admin/sei
-cd /home/sxt-admin/sei
+sudo chown -R sxt-admin:sxt-admin /home/sxt-admin/demo-shell-script
+sudo chmod -R 775 /home/sxt-admin/demo-shell-script
+sudo chown -R sxt-admin:sxt-admin /home/sxt-admin/demo-shell-script/sei
+sudo chmod -R 775 /home/sxt-admin/demo-shell-script/sei
+cd /home/sxt-admin/demo-shell-script/sei
 git checkout v3.1.1
